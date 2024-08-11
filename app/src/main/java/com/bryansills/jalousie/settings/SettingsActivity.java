394c6fffa5936ec5c29dd6a0c6a0255884eb3766
Guy Luz <guyluz11@gmail.com>
2024-08-11 19:59:43 +0300

    Fixing build

--- app/src/main/java/com/bryansills/jalousie/settings/SettingsActivity.java
+++ app/src/main/java/com/bryansills/jalousie/settings/SettingsActivity.java
@@ -1,14 +1,14 @@
 package com.bryansills.jalousie.settings;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.bryansills.jalousie.R;
 
 /**
  * Created by bsills on 11/20/14.
  */
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
